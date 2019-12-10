0610092cb67771a0ecb1a1a85cee206c6673fbe4
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-12-10 09:43:02 -0500

    bubbleSort completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,78 +3,81 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-       // write your code here
+        // write your code here
         int size = 10;
         int lowestNumber = 0;
         int highestNumber = 10;
 
 
-         int[] array = createNumberArray(size, lowestNumber, highestNumber);
-         //Pattern1();
-         //Pattern2();
-         //Pattern3();
-         //Pattern4();
-         //Pattern5();
-        
+        int[] array = createNumberArray(size, lowestNumber, highestNumber);
+        //Pattern1();
+        //Pattern2();
+        //Pattern3();
+        //Pattern4();
+        //Pattern5();
+
 //        System.out.println("The sum of the array is " + sum(array));
 //        System.out.println("The minimum of the array is " + minimum(array));
-         printArray(array);
-        System.out.println("The maximum of the array is " + maximum(array));
+        printArray(array);
+        //System.out.println("The maximum of the array is " + maximum(array));
+
+        System.out.println("After Bubble Sort");
+        printArray(bubbleSort(array));
     }
 
-    public static void Pattern1(){
+    public static void Pattern1() {
 
-           for (int times =0; times < 6; times++) {
+        for (int times = 0; times < 6; times++) {
 
-               for (int columns =0; columns <6; columns++){
-                   System.out.print("*");
-               }
-               System.out.println();
-           }
+            for (int columns = 0; columns < 6; columns++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
-           }
-    public static void Pattern2(){
+    }
 
-        for (int times =1; times < 7; times++) {
+    public static void Pattern2() {
 
-            for (int columns =1; columns <8; columns++){
+        for (int times = 1; times < 7; times++) {
+
+            for (int columns = 1; columns < 8; columns++) {
                 System.out.print(times);
             }
             System.out.println();
         }
 
 
-
     }
 
-    public static void Pattern3(){
+    public static void Pattern3() {
 
-        for (int times =1; times < 7; times++) {
+        for (int times = 1; times < 7; times++) {
 
-            for (int columns =1; columns <7; columns++){
+            for (int columns = 1; columns < 7; columns++) {
                 System.out.print(columns);
             }
             System.out.println();
         }
     }
 
-    public static void Pattern4(){
+    public static void Pattern4() {
 
-        for (int times =1; times <7; times++) {
+        for (int times = 1; times < 7; times++) {
 
-            for (int columns= 1; columns <=times; columns++){
+            for (int columns = 1; columns <= times; columns++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    public static void Pattern5(){
+    public static void Pattern5() {
 
-        for (int times =1; times <7; times++) {
+        for (int times = 1; times < 7; times++) {
 
-            for (int columns= 1; columns <=times; columns++){
+            for (int columns = 1; columns <= times; columns++) {
                 System.out.print(columns);
             }
             System.out.println();
@@ -83,19 +86,19 @@ public class Main {
 
 
     public static int sum(int[] array) {
-        int sum=0;
+        int sum = 0;
 
-        for(int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         return sum;
     }
 
     public static int minimum(int[] array) {
-        int minimum=array[0];
+        int minimum = array[0];
 
-        for(int i=0; i<array.length; i++) {
-            if (minimum<array[i]) {
+        for (int i = 0; i < array.length; i++) {
+            if (minimum < array[i]) {
                 array[i] = minimum;
             }
 
@@ -105,10 +108,10 @@ public class Main {
     }
 
     public static int maximum(int[] array) {
-        int maximum=array[0];
+        int maximum = array[0];
 
-        for(int i=0; i<array.length; i++) {
-            if (maximum>array[i]) {
+        for (int i = 0; i < array.length; i++) {
+            if (maximum > array[i]) {
                 array[i] = maximum;
             }
 
@@ -116,21 +119,30 @@ public class Main {
         }
         return maximum;
     }
-    public static int bubbleSort(int[] array) {
-        int maximum=array[2];
 
-        for(int i=1; i<array.length; i++) {
-            for (int j=0; j<array.length-1; j++) {
-               if (array[i]>array[j]) {
-                   int temp= array[j];
-                   array[j]= array[i];
-               }
+    public static int[] bubbleSort(int[] array) {
+
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i+1; j < array.length; j++) {
+
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                }
+
             }
 
-            maximum = array[i];
+
         }
-        return maximum;
+
+
+        return array;
+
     }
+
     public static int[] createNumberArray(int size, int lowestNumber, int highestNumber) {
         int[] array = new int[size];
 
@@ -153,8 +165,6 @@ public class Main {
         int number = (int) ((Math.random() * (highestNumber - lowestNumber + 1)) + lowestNumber);
         return number;
     }
-
-
 
 
 }
